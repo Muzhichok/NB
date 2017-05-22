0b048ffa25915a50d5e965029c668a2d9021ec66
o.muzhychok <o.muzhychok@hexa.com.ua>
2017-05-22 16:03:20 +0300

    first test for authorization

--- selenium/start-selenium.sh
+++ selenium/start-selenium.sh
@@ -2,6 +2,6 @@
 
 case $1 in
     'firefox') nohup java -jar selenium-server-standalone-2.52.0.jar > /dev/null 2>&1 &;;
-    *) nohup java -jar selenium-server-standalone-2.52.0.jar -Dwebdriver.chrome.driver=../chromerdriver/chromedriver > /dev/null 2>&1 &;;
+    *) nohup java -jar selenium-server-standalone-2.52.0.jar > /dev/null 2>&1 &;;
 esac
 
--- tests/acceptance/Authorization/LoginCept.php
+++ tests/acceptance/Authorization/LoginCept.php
@@ -2,7 +2,19 @@
 
 $I = new AcceptanceTester($scenario);
 
-$I->wantTo('perform actions and see result');
+$I->wantTo('login as admin');
+
+$admin = [
+    'username' => 'Admin',
+    'password' => 'admin'
+];
 
 $I->amOnPage('/');
+
+$I->fillField(".//*[@id='txtUsername']", $admin['username']);
+$I->fillField(".//*[@id='txtPassword']", $admin['password']);
+$I->click(".//*[@id='btnLogin']");
+
+$I->canSee('Welcome Admin');
+
 
