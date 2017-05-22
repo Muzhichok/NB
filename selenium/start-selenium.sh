a134fe36457aaaccb8828959d14e0744b6cf4b98
o.muzhychok <o.muzhychok@hexa.com.ua>
2017-05-22 14:19:45 +0300

    test project structure

---
+++ selenium/start-selenium.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+case $1 in
+    'firefox') nohup java -jar selenium-server-standalone-2.52.0.jar > /dev/null 2>&1 &;;
+    *) nohup java -jar selenium-server-standalone-2.52.0.jar -Dwebdriver.chrome.driver=../chromerdriver/chromedriver > /dev/null 2>&1 &;;
+esac
+
